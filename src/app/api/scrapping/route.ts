14ef60ff68ec1111ded45d54e354b21ffb8ceec7
FranCant <cantorifrancisco@gmail.com>
2024-09-08 13:06:37 +0200

    ok

--- src/app/api/scrapping/route.ts
+++ src/app/api/scrapping/route.ts
@@ -1,47 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
-import puppeteer from "puppeteer";
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 export async function GET(req: NextRequest) {
   const urlParams = req.nextUrl.searchParams.get("url");
 
-  let browser;
-  try {
-    // Lanzar una instancia de Puppeteer
-    browser = await puppeteer.launch({
-      headless: true,
+  if (!urlParams) {
+    return NextResponse.json({
+      error: "URL parameter is missing",
+      status: 400,
     });
+  }
 
-    const page = await browser.newPage();
-    await page.goto(urlParams ?? "", {
-      waitUntil: "networkidle2",
-    });
+  // Reemplaza con tu propia API Key
+  const apiKey = "8e7eeb199e46d97955e1f4d55bab99c1";
+  const scraperApiUrl = `http://api.scraperapi.com?api_key=${apiKey}&url=${urlParams}`;
+
+  try {
+    // Realizar la solicitud a Scraper API
+    const response = await axios.get(scraperApiUrl);
+    const html = response.data;
+
+    // Cargar el HTML con cheerio
+    const $ = cheerio.load(html);
 
     // Extraer datos de todos los productos del DOM de la página
-    const products = await page.evaluate(() => {
-      const productElements = document.querySelectorAll(
-        ".js-product-miniature-wrapper"
-      );
-      return Array.from(productElements).map((product) => {
-        const title =
-          (
-            product.querySelector(".product-title") as HTMLElement
-          )?.textContent?.trim() || "";
+    const products = $(".js-product-miniature-wrapper")
+      .map((_, product) => {
+        const title = $(product).find(".product-title").text().trim() || "";
         const reference =
-          (
-            product.querySelector(".product-reference") as HTMLElement
-          )?.textContent?.trim() || "";
+          $(product).find(".product-reference").text().trim() || "";
         const price =
-          (
-            product.querySelector(
-              ".product-price-and-shipping span"
-            ) as HTMLElement
-          )?.textContent?.trim() || "";
+          $(product).find(".product-price-and-shipping span").text().trim() ||
+          "";
         return { title, reference, price };
-      });
-    });
-
-    // Cerrar el navegador
-    await browser.close();
+      })
+      .get();
 
     // Responder con los datos JSON
     return NextResponse.json({
@@ -49,7 +43,6 @@ export async function GET(req: NextRequest) {
       data: products,
     });
   } catch (error) {
-    if (browser) await browser.close();
     console.error("Error scraping the site:", error.message);
     return NextResponse.json({
       error: error.message || "Error scraping the site",
